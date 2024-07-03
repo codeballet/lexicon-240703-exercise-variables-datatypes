f76d23b83348e4814a17ffb8e9663e2fcffc8baf
stjernholm <stjernholm@msn.com>
2024-07-03 14:01:54 +0200

    Tasks complete

---
+++ script.js
@@ -0,0 +1,79 @@
+// Exercise 0
+console.log("\nExercise 0:");
+console.log("Hello world!");
+
+// Exercise 1
+console.log("\nExercise 1:");
+let age = 18;
+console.log(`This is my age: ${age}`);
+
+// Exercise 2
+console.log("\nExercise 2:");
+var oldSchool = "Don't use this anymore!";
+let newWay = "Use this to declare a variable that may change.";
+const notChanging = "This type is used for stuff that do not change.";
+console.log(`Declaration 'var': ${oldSchool}`);
+console.log(`Declaration 'let': ${newWay}`);
+console.log(`Declaration 'const': ${notChanging}`);
+
+// Exercise 3
+console.log("\nExercise 3:");
+let sunny = true;
+let weather = "cloudy";
+let height = "10";
+console.log(`sunny: ${sunny}`);
+console.log(`weather: ${weather}`);
+console.log(`height: ${height}`);
+
+// Exercise 4
+console.log("\nExercise 4:");
+const name = "Johan";
+
+try {
+    name = "Other";
+} catch (error) {
+    console.log(error);
+}
+
+// Exercise 5
+console.log("\nExercise 5:");
+console.log("Booleans may be used for logic in 'if' statements.");
+
+// Exercise 6
+console.log("\nExercise 6:");
+console.log(
+    "We should use 'const', preventing accidental changes of the variable"
+);
+
+// Exercise 7
+console.log("\nExercise 7:");
+let variable1 = 2;
+let variable2 = 3;
+let result = variable1 + variable2;
+console.log(`Result: ${result}`);
+
+// Exercise 8
+console.log("\nExercise 8:");
+let result1 = variable1 - variable2;
+let result2 = variable1 * variable2;
+let result3 = variable1 / variable2;
+console.log(`result1: ${result1}`);
+console.log(`result2: ${result2}`);
+console.log(`result3: ${result3}`);
+
+// Exercise 9
+console.log("\nExercise 9:");
+let a = 1;
+a += 5;
+console.log(a);
+
+// Exercise 10
+console.log("\nExercise 10:");
+console.log("Here, 'a' is equal to 9");
+
+// Exercise 11
+console.log("\nExercise 11:");
+console.log("Line 1 assigns the value 10 to variable 'a'.");
+console.log("Line 2 subtracts 4 from a, leaving it as 6. ");
+console.log("Line 3 assigns the new value 7 to a.");
+console.log("Line 4 adds 2 to a, leaving it as 9.");
